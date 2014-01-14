34ccd7f06f9101846f77dbeb2ab05fc5f64a6cbf
Lala <gingkoginkgo@hotmail>
2014-01-14 21:39:58 +0800

    still define WMSFile

--- project01/WMSFile.h
+++ project01/WMSFile.h
@@ -2,21 +2,24 @@
 #define WMSFILE_H
 
 using namespace std;
-
-class WMSFile
-{
-
-
 /*To-Do:
  * 01.Can get user input file path
  * 02.Can create new xml file
  * 03.Can load new xml file
  * 04.Can Detect error about :
- *    a.file path
+ *    a.file path syntax error
  *    b.file already exists when create new file
+ *    c.when add item, price and stock can not be negative or NaN(not a number)
+ *    d.can not load file before create file.
+ *    e.can not add item and print in screen before load file.
  * 05.Can write somthings to file
  * 06.Can read somthings from file and print in screen
- * /
+ */
+
+class WMSFile
+{
+
+
 
 };
 
